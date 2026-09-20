a5d1f59d9f6481fd54b5ba9adb086af4763f6dfd
Damián Búho <damian.buho@proton.me>
2026-09-19 21:22:37 -0300

    refactor: split O9S_NGINX_COMPRESS_TYPES into category variables
    - Add 6 category ENV vars with defaults in Dockerfile:
      O9S_NGINX_COMPRESS_TYPES_TEXT, _JAVASCRIPT, _JSON, _XML, _FONTS, _IMAGES
    - Add entrypoint script 0700-compute-compress-types.sh that composes
      O9S_NGINX_COMPRESS_TYPES from categories before template rendering
    - Users can override individual categories without rebuilding full list
    - Templates continue using {{ ENV.O9S_NGINX_COMPRESS_TYPES }}

---
+++ .container/user/entrypoint.d/0700-compute-compress-types.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2026 Damián Búho <damian.buho@proton.me>
+#
+# SPDX-License-Identifier: MIT
+
+# Compute O9S_NGINX_COMPRESS_TYPES from category variables
+# Runs before template rendering (1000-parallel-j2.sh)
+
+set -euo pipefail
+
+# shellcheck source=.container/foundation/tools.d/b19-i18n
+# shellcheck disable=SC1091
+. b19-i18n
+b19-log debug "COMPRESS_TYPES" "computing from category variables"
+
+# Category variables with defaults matching Dockerfile
+COMPRESS_TYPES_TEXT="${O9S_NGINX_COMPRESS_TYPES_TEXT:-text/css text/plain text/xml text/x-component}"
+COMPRESS_TYPES_JAVASCRIPT="${O9S_NGINX_COMPRESS_TYPES_JAVASCRIPT:-application/javascript application/x-javascript text/javascript}"
+COMPRESS_TYPES_JSON="${O9S_NGINX_COMPRESS_TYPES_JSON:-application/json application/manifest+json application/vnd.api+json}"
+COMPRESS_TYPES_XML="${O9S_NGINX_COMPRESS_TYPES_XML:-application/atom+xml application/rss+xml application/xml+rss application/xhtml+xml application/xml}"
+COMPRESS_TYPES_FONTS="${O9S_NGINX_COMPRESS_TYPES_FONTS:-application/vnd.ms-fontobject application/x-font-opentype application/x-font-truetype application/x-font-ttf font/eot font/opentype font/otf font/ttf}"
+COMPRESS_TYPES_IMAGES="${O9S_NGINX_COMPRESS_TYPES_IMAGES:-image/svg+xml image/vnd.microsoft.icon image/x-icon}"
+
+# Combine all categories
+COMPRESS_TYPES="${COMPRESS_TYPES_TEXT} ${COMPRESS_TYPES_JAVASCRIPT} ${COMPRESS_TYPES_JSON} ${COMPRESS_TYPES_XML} ${COMPRESS_TYPES_FONTS} ${COMPRESS_TYPES_IMAGES}"
+
+# Export for template rendering
+export O9S_NGINX_COMPRESS_TYPES="${COMPRESS_TYPES}"
+
+b19-log debug "COMPRESS_TYPES" "computed: ${O9S_NGINX_COMPRESS_TYPES}"
